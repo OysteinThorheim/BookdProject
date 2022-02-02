044c8d658b86d23d8d1f0e4035de60e0f997ffbb
OysteinThorheim <mr.dolheim@gmail.com>
2022-02-02 09:46:11 +0100

    Initial Commit

---
+++ BookdProject/BookdProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookdProjectApp.swift
+//  BookdProject
+//
+//  Created by Øystein Thorheim on 02/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct BookdProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
